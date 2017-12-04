3fec4ea26630ec1721ea1f4821f0b0a100ff7840
akustix <tyler.mcdermott88@gmail.com>
2017-12-04 18:08:44 -0500

    I added an explosion and tried to set up the scripting (Was able to get the audio attatched the file) Nothing else was edited or saved. G

---
+++ Assets/Scripts/Merchant/LgExplosion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LgExplosion : MonoBehaviour
+{
+	public AudioClip clip;
+
+	private AudioSource source;
+
+    public void Start()
+	{
+		this.source = this.GetComponent<AudioSource>();
+	}
+
+	private void Destroy()
+	{
+		TrashMan.despawn(this.gameObject);
+	}
+}
