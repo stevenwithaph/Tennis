d068c72d37580768496eac99d1ec50f0e9a8292a
akustix <tyler.mcdermott88@gmail.com>
2017-12-04 20:01:07 -0500

    added explosion animation and sound to go with it. - Added a death sound to play at game over (?) - Implimented start sound on game start. Fixed pressed start anim.

---
+++ Assets/Scripts/Merchant/audiostart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audiostart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+ 	void Update()
+ 	{ 
+  	// If the left mouse button is pressed down...
+  	if(Input.GetMouseButtonDown(0) == true)
+  	{
+   	GetComponent<AudioSource>().Play();
+  	} 
+ 	}
+}
+
