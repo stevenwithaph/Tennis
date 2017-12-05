7fcadad87d773a9175520631ee2910be8a7555bb
Steven Adams <stevenadams@Stevens-MacBook-Pro.local>
2017-12-04 20:30:45 -0500

    Audio start

--- Assets/Scripts/Merchant/audiostart.cs
+++ Assets/Scripts/Merchant/audiostart.cs
@@ -2,21 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class audiostart : MonoBehaviour {
+public class audiostart : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
- 	void Update()
- 	{ 
-  	// If the left mouse button is pressed down...
-  	if(Input.GetMouseButtonDown(0) == true)
-  	{
-   	GetComponent<AudioSource>().Play();
-  	} 
- 	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the left mouse button is pressed down...
+        if (Input.GetMouseButtonDown(0) == true)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
 }
 
